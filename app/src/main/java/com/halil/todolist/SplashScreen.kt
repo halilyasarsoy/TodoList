540d4ce82ad1d48c224f7da5051fba6214726138
Halil Yasarsoy <71354610+halilyasarsoy@users.noreply.github.com>
2022-09-02 13:57:14 +0300

    Splash activity done

---
+++ app/src/main/java/com/halil/todolist/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.halil.todolist
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+
+            )
+        Handler().postDelayed({
+            val intent = Intent (this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },4000)
+
+    }
+}
+
+
